3a35196914c72d4d5067f1cd5af9112b7b786496
Miu Kobayashi <miukoba@gmail.com>
2021-01-23 22:34:11 +0900

    startとstopのアイコンを変更

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-const { app, Tray, Menu, MenuItem } = require('electron')
-const ICON_PATH = `${__dirname}/img/icon.png`
+const { app, dialog, Tray, Menu, MenuItem } = require('electron')
+const ICON_PATH_PLAYING = `${__dirname}/img/icon_playing.png`
+const ICON_PATH_STOPPING = `${__dirname}/img/icon_stopping.png`
+
+let tray
+
+const startMusic = () => {
+  tray.setImage(ICON_PATH_PLAYING)
+}
+
+const stopMusic = () => {
+  tray.setImage(ICON_PATH_STOPPING)
+}
 
 app.on('ready', () => {
-  const appIcon = new Tray(ICON_PATH)
+  tray = new Tray(ICON_PATH_STOPPING)
   const menu = new Menu()
 
   menu.append(new MenuItem({
-    label: 'item',
+    label: 'start BGM',
     click: () => {
-      alert('click item')
+      startMusic()
+      dialog.showErrorBox('エラー1', 'start clicked!')
+    },
+  }))
+  menu.append(new MenuItem({
+    label: 'stop BGM',
+    click: () => {
+      stopMusic()
+      dialog.showErrorBox('エラー2', 'stop clicked!')
     },
   }))
   menu.append(new MenuItem({ type: 'separator' }))
   menu.append(new MenuItem({ role: 'quit' }))
 
-  appIcon.setContextMenu(menu)
+  tray.setContextMenu(menu)
 })
 
 app.on('ready', () => {
