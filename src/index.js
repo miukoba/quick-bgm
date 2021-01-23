d703ccb94bf2f69c2f5aaab1fd435d7cab450423
Miu Kobayashi <miukoba@gmail.com>
2021-01-24 00:37:52 +0900

    一旦完成

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-const { app, dialog, Tray, Menu, MenuItem } = require('electron')
+const { app, BrowserWindow, Tray, Menu, MenuItem } = require('electron')
+
 const ICON_PATH_PLAYING = `${__dirname}/img/icon_playing.png`
 const ICON_PATH_STOPPING = `${__dirname}/img/icon_stopping.png`
 
+let mainWindow
 let tray
-
-const startMusic = () => {
-  tray.setImage(ICON_PATH_PLAYING)
-}
-
-const stopMusic = () => {
-  tray.setImage(ICON_PATH_STOPPING)
-}
 
 app.on('ready', () => {
   tray = new Tray(ICON_PATH_STOPPING)
@@ -18,24 +12,48 @@ app.on('ready', () => {
 
   menu.append(new MenuItem({
     label: 'start BGM',
-    click: () => {
-      startMusic()
-      dialog.showErrorBox('エラー1', 'start clicked!')
-    },
+    click: () => { startMusic() },
   }))
   menu.append(new MenuItem({
     label: 'stop BGM',
-    click: () => {
-      stopMusic()
-      dialog.showErrorBox('エラー2', 'stop clicked!')
-    },
+    click: () => { stopMusic() },
   }))
   menu.append(new MenuItem({ type: 'separator' }))
   menu.append(new MenuItem({ role: 'quit' }))
 
   tray.setContextMenu(menu)
+
+  createWindow()
 })
 
 app.on('ready', () => {
   app.dock.hide()
 })
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    // width: 100, height: 800, webPreferences: {
+    width: 0, height: 0, webPreferences: {
+      nodeIntegration: true, // FIXME セキュリティ的に良くない
+    },
+  })
+  mainWindow.loadFile(`${__dirname}/index.html`)
+  // mainWindow.webContents.openDevTools()
+}
+
+const startMusic = () => {
+  tray.setImage(ICON_PATH_PLAYING)
+
+  if (mainWindow !== null) {
+    mainWindow.webContents.send('play-bgm')
+  }
+}
+
+const stopMusic = () => {
+  tray.setImage(ICON_PATH_STOPPING)
+
+  if (mainWindow !== null) {
+    mainWindow.webContents.send('stop-bgm')
+  }
+
+}
